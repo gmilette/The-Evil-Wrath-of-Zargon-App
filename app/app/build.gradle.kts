ec6cb30e71cce7a0fe22715efc3a39d69a1a6806
“gmilette” <gregorym@gmail.com>
2025-12-27 16:05:12 -0500

    update version for release

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.greenopal.zargon"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
